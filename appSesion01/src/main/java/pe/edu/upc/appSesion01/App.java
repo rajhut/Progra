d56ba10fe08869a689e420dc068793bade0d1527
Henry Mendoza Puerta <hmendo81@gmail.com>
2016-08-15 17:32:40 -0500

    Agregamos codigo sobre sintaxis de arreglos, objetos.

---
+++ appSesion01/src/main/java/pe/edu/upc/appSesion01/App.java
@@ -0,0 +1,38 @@
+package pe.edu.upc.appSesion01;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	//Clase objeto=new Constructor()    	
+    	Scanner s=new Scanner(System.in);    	
+    	
+    	System.out.println( "Ejercicio 1" );
+    	String[] arrNombre;    	
+    	arrNombre=new String[3];
+    	
+    	//tipos de datos basico/primitivos
+    	int numero;
+    	int[] valores;
+    	double monedas;
+    	
+    	//Wrapper( Clases Envolventes)==> 
+    	//Casting ( Conversion de tipos de datos ) 
+    	Integer valores_2=new Integer(1);
+    	Double x=new Double(10);
+    	
+    	//Estructuras de control
+    	for (int i = 0; i < arrNombre.length; i++) {
+		   System.out.print("Ingrese un nombre"+(i+1));
+		   arrNombre[i]=s.nextLine();
+		}
+    	
+        
+    }
+}
