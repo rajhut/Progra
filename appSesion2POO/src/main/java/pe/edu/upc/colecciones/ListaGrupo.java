097d8f783e709152cf59eda9fa9b8cdb16e9ba07
victor.alex.jimenez.chauca@hotmail.com <victor.alex.jimenez.chauca@hotmail.com>
2016-08-16 17:57:24 -0500

    Segunda PArte

---
+++ appSesion2POO/src/main/java/pe/edu/upc/colecciones/ListaGrupo.java
@@ -0,0 +1,27 @@
+package pe.edu.upc.colecciones;
+
+import java.util.ArrayList;
+
+import pe.edu.upc.entitys.Grupo;
+
+public class ListaGrupo {
+	private ArrayList<Grupo> lGrupo;
+
+	public ListaGrupo() {
+		lGrupo = new ArrayList<>();
+	}
+
+	
+	public String insGrupo(Grupo oGrupo) {
+		return null;
+	}
+
+	public int cntGrupo() {
+		return 0;
+	}
+
+	public Grupo elemento(int pos) {
+		return null;
+	}
+
+}
